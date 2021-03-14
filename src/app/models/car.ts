578fe57a3e1615bd77445c8b21f23c2e0dc8b3ca
Veysel Himmetoğlu <veyselhim@gmail.com>
2021-03-14 19:26:23 +0300

    Angular day 2

---
+++ src/app/models/car.ts
@@ -0,0 +1,6 @@
+export interface Car{
+    carId:number;
+    carName:string;
+    brandName:string;
+    colorName:string;
+}
---
+++ src/app/services/color.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BrandResponseModel } from '../models/brandResponseModel';
+import { ColorResponseModel } from '../models/colorResponseModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ColorService {
+
+
+  apiUrl = "https://localhost:44352/api/colors/getall";
+
+  constructor(private httpClient: HttpClient) { }
+
+  getColors():Observable<ColorResponseModel> {
+    return this.httpClient.get<ColorResponseModel>(this.apiUrl);
+  }
+}
